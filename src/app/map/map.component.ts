152da94f708a82a57806e45fafc68f53a707cddd
Daniele Monaldi <daniele-monaldi@hotmail.it>
2024-02-03 19:29:32 +0100

    Reorganized code with methods.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,7 +3,6 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import * as L from 'leaflet';
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {PoiInfoModalComponent} from "./poi-info-modal/poi-info-modal.component";
-import {marker} from "leaflet";
 
 @Component({
   selector: 'app-map',
@@ -17,13 +16,22 @@ export class MapComponent implements OnInit {
   constructor(private http: HttpClient, private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    const map = this.initializeMap();
+    this.loadBordersGeoJson(map);
+    this.loadPoiMarkers(map);
+  }
 
+  private initializeMap(): L.Map {
     const map = L.map('map').setView([43.1608, 13.7182], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
 
+    return map;
+  }
+
+  private loadBordersGeoJson(map: L.Map): void {
     this.http.get('/assets/data/borders.geojson').subscribe(
       (data: any) => {
         L.geoJson(data, {
@@ -38,21 +46,27 @@ export class MapComponent implements OnInit {
         console.error('Errore nel caricamento del GeoJSON:', error);
       }
     );
+  }
 
+  private loadPoiMarkers(map: L.Map): void {
     this.http.get('api/POI/getAllPOI').subscribe(
       (data: any) => {
         data.forEach((dataItem: any) => {
           const marker = L.marker([dataItem.latitude, dataItem.longitude]).addTo(map);
-          marker.on('click', () => {
-            const modalRef = this.modalService.open(PoiInfoModalComponent);
-            modalRef.componentInstance.modalTitle = dataItem.name;
-            modalRef.componentInstance.description = dataItem.description;
-          })
+          this.setupMarkerClickEvent(marker, dataItem);
         });
       },
       (error) => {
         console.error('Errore nella richiesta HTTP:', error);
       }
     );
+  }
+
+  private setupMarkerClickEvent(marker: L.Marker, dataItem: any): void {
+    marker.on('click', () => {
+      const modalRef = this.modalService.open(PoiInfoModalComponent);
+      modalRef.componentInstance.modalTitle = dataItem.name;
+      modalRef.componentInstance.description = dataItem.description;
+    });
   }
 }
