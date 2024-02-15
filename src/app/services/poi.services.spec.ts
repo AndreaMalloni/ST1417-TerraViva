8778755564a9eef591099b24cc6e17c61ea204b1
Daniele Monaldi <daniele-monaldi@hotmail.it>
2024-02-15 23:08:08 +0100

    Added POI services class. Code refactor.

---
+++ src/app/services/poi.services.spec.ts
@@ -0,0 +1,10 @@
+import { PoiServices } from './poi.services';
+
+describe('PoiServices', () => {
+
+  let service: PoiServices;
+
+  it('should create an instance', () => {
+    expect(service).toBeTruthy();
+  });
+});
