f48e7b05a137cf779c41b4306e9a1e959d0db39c
Andrea Malloni <malloni.andrea02@gmail.com>
2024-02-22 11:39:31 +0100

    Refactored interceptor url controls

--- src/app/interceptors/token-setter.interceptor.ts
+++ src/app/interceptors/token-setter.interceptor.ts
@@ -2,11 +2,19 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import {environment} from "../../environments/environment.development";
 
 export const tokenSetterInterceptor: HttpInterceptorFn = (req, next) => {
-  if((req.url != environment.baseURL + "/api/login") &&
-    req.url != environment.baseURL + "/api/register") {
-    req = req.clone({ setHeaders: {'Authorization': `Bearer ${sessionStorage.getItem("token")}`} })
-    console.log(req.headers.get("Authorization"));
+  let tokenNotNeeded = [
+    environment.baseURL + "/api/login",
+    environment.baseURL + "/api/register",
+    environment.baseURL + "/api/POI/getAllPOI"
+  ]
+
+  for (const url in tokenNotNeeded) {
+    if(req.url == url) {
+      return next(req);
+    }
   }
 
+  req = req.clone({ setHeaders: {'Authorization': `Bearer ${sessionStorage.getItem("token")}`} })
+  console.log(req.headers.get("Authorization"));
   return next(req);
 };
