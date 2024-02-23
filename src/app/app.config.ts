260f9253cba18f97f26f979e5ec5bb1677f305d2
Andrea Malloni <malloni.andrea02@gmail.com>
2024-02-23 11:03:48 +0100

    Added PWA feature.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {tokenSetterInterceptor} from "./interceptors/token-setter.interceptor";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withInterceptors([tokenSetterInterceptor]))]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([tokenSetterInterceptor])), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
